7a338d1429a9c7112e2f8dd15cabd7a8e43a280b
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-03-02 12:11:24 -0300

    vercel back

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,13 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URL
 } = process.env;
 
 const DietModel = require("./models/Diet")
 const RecipeModel = require("./models/Recipe")
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}` || `${DB_URL}` , {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
